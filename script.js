4581c70a1be968f7a2d23b53f9d07201a0b3261c
educanovomundo <admin@educa.novomundo.mt.gov.br>
2024-02-17 14:32:16 -0300

    ADD Aluno na tabela

--- script.js
+++ script.js
@@ -1,35 +1,90 @@
-
-
- 
  const btnLoc = document.querySelector('#btn-localizacao')
  const textLatLong = document.querySelector('#textLatLong')
+ const nomeResponsavel = document.querySelector('#nome');
+ const enderecoResponsavel = document.querySelector('#endereco');
+ const linhaResponsavel = document.querySelector('#linha');
+ const btnEnviar = document.querySelector('#btnEnviar'); 
+ var localiza = ""
+
+// Capturar variáveis do documento
+ let inputNomeAluno = document.querySelector('#InputNomeAluno')
+ let inputEscolaAluno = document.querySelector('#InputEscola')
+ let inputSerieAluno = document.querySelector('#InputSerie')
+ let tableAlunos = document.querySelector('#tableAlunos')
+ let btnNovoAluno = document.querySelector('#btnNovoAluno')
+ let listaAlunos = []
+
  
- 
- 
+
+// botão Nova Tarefa
+btnNovoAluno.addEventListener('click', (e)=>{
+    
+    let aluno = {
+        nome: inputNomeAluno.value,
+        escola: inputEscolaAluno.value,
+        serie: inputSerieAluno.value,
+        id: gerarID()
+    }
+
+    if(!inputNomeAluno.value){
+        alert('Informe o nome do aluno')
+        return
+    }
+    
+    if(!inputEscolaAluno.value){
+        alert('Informe a escola do aluno')
+        return
+    }
+    
+    if(!inputSerieAluno.value){
+        alert('Informe a série do aluno')
+        return
+    }
+
+    addAluno(aluno);
+    inputNomeAluno.value = "";
+    inputEscolaAluno.value = "";
+    inputSerieAluno.value = "";
+} )
+
+
+
+// gerar ID personalizado
+function gerarID(){
+    return Math.floor(Math.random() * 300);
+}
+
+
+//adicionar aluno
+function addAluno(aluno){
+    let tr = criarTagTr(aluno);
+    tableAlunos.innerHTML += tr
+    listaAlunos.push(aluno);
+}
+
+
+function criarTagTr(aluno){
+    let registro = ''
+    registro = `
+    <tr>
+        <td>${aluno.nome}</td>
+        <td>${aluno.escola}</td>
+        <td>${aluno.serie}</td>
+    </tr>
+    `
+    console.log(registro)
+    return registro;
+    
+}
+
+
  btnLoc.onclick = () => getLocation()
 
+ btnEnviar.onclick= () => { 
 
-
-
-// // const name = document.querySelector(".name");
-// // const btn = document.querySelector("button");
-
-// //       btn.addEventListener("click", function () {
-// //         name.style.color = "blue";
-// //  });
-
-
-// // function addAluno(){
-
-// // }
-
-
-
-
-
-
-
-
+    console.log(listaAlunos);
+    
+ }
 
 
 
@@ -50,7 +105,8 @@ function showPosition(position)
   lon=position.coords.longitude;
   console.log(position.coords.latitude)
   console.log(position.coords.longitude)
-  textLatLong.value = `Lat: ${position.coords.latitude}, ${position.coords.longitude}`
+  localiza = `Lat: ${position.coords.latitude}, Long: ${position.coords.longitude}`
+  document.querySelector('#textLatLong').value = localiza;
   
   }
 
