ac2b2262679f0bd2b4f35eff95e26b3ea7517ee1
educanovomundo <admin@educa.novomundo.mt.gov.br>
2024-02-19 02:04:59 -0300

    Envio cadastro para Zap

--- script.js
+++ script.js
@@ -15,15 +15,30 @@
  let listaAlunos = []
 
  
+ //Chamar a função getLocation automaticamente
+getLocation()
 
-// botão Nova Tarefa
+btnEnviar.style.display = "none";
+
+
+function obterResponsavel(){
+    let responsavel = {
+        nome: nomeResponsavel.value,
+        endereco: enderecoResponsavel.value,
+        linha: linhaResponsavel.value,
+        localizacao: localiza
+    }
+}
+
+
+// botão Novo Aluno
 btnNovoAluno.addEventListener('click', (e)=>{
     
     let aluno = {
         nome: inputNomeAluno.value,
         escola: inputEscolaAluno.value,
         serie: inputSerieAluno.value,
-        id: gerarID()
+        id: gerarID(),
     }
 
     if(!inputNomeAluno.value){
@@ -42,6 +57,7 @@ btnNovoAluno.addEventListener('click', (e)=>{
     }
 
     addAluno(aluno);
+    btnEnviar.style.display = "block";
     inputNomeAluno.value = "";
     inputEscolaAluno.value = "";
     inputSerieAluno.value = "";
@@ -77,15 +93,93 @@ function criarTagTr(aluno){
     
 }
 
-//Chamar a função getLocation automaticamente
-getLocation()
+
 
 
 //  btnLoc.onclick = () => getLocation()
 
- btnEnviar.onclick= () => { 
+ btnEnviar.onclick= () => {
+    
+    
+    // VALIDAÇÃO
 
-    console.log(listaAlunos);
+    // Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+          event.preventDefault();
+
+
+          
+        let responsavel = {
+            nome: nomeResponsavel.value,
+            endereco: enderecoResponsavel.value,
+            linha: linhaResponsavel.value,
+            localizacao: localiza,
+            alunos: listaAlunos
+        }
+            
+            enviarParaWhatsApp(responsavel)
+
+        }, false)
+      })
+  })()
+
+
+  //ENVIAR PARA ZAP  
+  function enviarParaWhatsApp(responsavel) {
+    var foneDestino = "5566992034311"
+
+    var text=encodeURIComponent(`*CADASTRO TRANSPORTE ESCOLAR-2024*
+    
+    `)
+
+    responsavel.alunos.forEach(aluno => {
+     
+    text += encodeURIComponent(`*Nome*: ${aluno.nome}
+     `)
+    text += encodeURIComponent(`*Escola*: ${aluno.escola}
+     `)
+    text += encodeURIComponent(`*Serie*: ${aluno.serie}
+     `)
+    text += encodeURIComponent(`*Responsável*: ${responsavel.nome}
+     `)
+     text += encodeURIComponent(`*Endereço*: ${responsavel.endereco}
+     `)
+     text += encodeURIComponent(`*Linha Ônibus*: ${responsavel.linha}
+     `)
+     text += encodeURIComponent(`*Coordenadas da Casa*: ${responsavel.localizacao}
+     `)
+     text += encodeURIComponent(`------------------------
+     `)
+
+
+    });
+
+    
+    
+     const url_redirect = `https://api.whatsapp.com/send?phone=${foneDestino}&text=${text}`
+
+     window.location.href = url_redirect;
+  } 
+    
+
+
+
+    
     
  }
 
